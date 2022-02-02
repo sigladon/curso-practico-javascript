d4da4037a80815a9fd389ca5a23f84c8ba235ca8
sigladon <sigladon.rc@gmail.com>
2022-02-02 15:52:57 -0500

    Escribiendo HTML desde JavaScript

--- descuentos.js
+++ descuentos.js
@@ -16,3 +16,15 @@
 function calcularPrecioDescuento(precio, descuento) {
     return precio * (100 - descuento) / 100;
 }
+
+function applyDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const inputDiscount = document.getElementById("inputDiscount");
+
+    const vinputPrice = Number(inputPrice.value);
+    const vinputDiscount = Number(inputDiscount.value);
+
+    const discount = calcularPrecioDescuento(vinputPrice, vinputDiscount);
+    const discountText = document.getElementById("pDiscount");
+    discountText.innerText = "El nuevo precio es $" + discount;
+}
